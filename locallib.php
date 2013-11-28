973fa26e0733f6df719dd7a06a0a2ff287d25b5a
Jan Hybš <x3mSpeedy@gmail.com>
2013-11-28 21:38:39 +0100

    add setting supporting classes, add comments, methods

--- locallib.php
+++ locallib.php
@@ -28,15 +28,62 @@
 
 defined ('MOODLE_INTERNAL') || die();
 
-/**
- * Does something really useful with the passed things
- *
- * @param array $things
- * @return object
- */
-//function codiana_do_something_useful(array $things) {
-//    return new stdClass();
-//}
+class codiana_state {
+
+    private static $prefix = 'codiana:state:';
+
+    private static $state = array (
+        '0' => 'waitingtoprocess',
+        '1' => 'correctsolution',
+        '2' => 'wrongsolution',
+        '3' => 'maxtimelimit',
+        '4' => 'maxmemorylimit',
+        '5' => 'compilationerror',
+        '6' => 'runerror',
+        '7' => 'looperror',
+        '8' => 'dangerouscode',
+        '9' => 'nomainclass',
+        '?' => 'unkwnownerror'
+    );
+
+
+
+    /**
+     * @param $value int state index
+     * @return string state name
+     */
+    public static function get ($value) {
+        return get_string (codiana_state::$prefix .
+                           (array_key_exists ($value, codiana_state::$state) ?
+                               codiana_state::$state[$value] :
+                               codiana_state::$state['?']
+                           ), 'codiana');
+    }
+}
+
+
+
+class codiana_display_options {
+
+    const OPEN_SOLVER = 0;
+
+    const CLOSE_SOLVER = 1;
+
+    const OPEN_OTHERS = 2;
+
+    const CLOSE_OTHERS = 3;
+
+    const COUNT = 4;
+
+    public static $types = array (
+        codiana_display_options::OPEN_SOLVER,
+        codiana_display_options::CLOSE_SOLVER,
+        codiana_display_options::OPEN_OTHERS,
+        codiana_display_options::CLOSE_OTHERS);
+
+    public static $fields = array ('username', 'state', 'runstat', 'resultstat', 'code');
+}
+
 
 
 /**
@@ -61,17 +108,31 @@ function codiana_check_int ($codiana, $prop, $required = true) {
 
 }
 
+/**
+ * @param $value string value
+ * @param null $default string default value, if $value is empty
+ * @return null|string string trimmed value or null/default value
+ */
 function codiana_parse_string ($value, $default = null) {
     $value = empty($value) ? $default : trim ($value);
     return $value;
 }
 
+/**
+ * @param $value int value
+ * @param null $default int default value, if $value is empty
+ * @return int|null int value format or null/default value
+ */
 function codiana_parse_int ($value, $default = null) {
     $value = empty($value) ? $default : intval ($value);
     return $value;
 }
 
-
+/**
+ * @param $value int value
+ * @param null $default int default value, if $value is empty
+ * @return int|null timestamp in int format or null/default value
+ */
 function codiana_parse_timestamp ($value, $default = null) {
     $value = empty($value) ? $default : intval ($value);
     return $value;
@@ -84,7 +145,11 @@ function codiana_solution_submit () {
 //    die ();
 }
 
-
+/**
+ * @param $mform mixed
+ * @param $name string name of input element with file
+ * @return string file extension
+ */
 function codiana_solution_get_extension ($mform, $name) {
     $filename = $mform->get_new_filename ($name);
     $extension = pathinfo ($filename, PATHINFO_EXTENSION);
@@ -94,7 +159,8 @@ function codiana_solution_get_extension ($mform, $name) {
 
 
 /**
- * @return IFileTransfer
+ * @return IFileTransfer instance of either LocalFileTransfer or RemoteFileTransfer
+ * (depends on 'codiana/islocal' config value )
  */
 function codiana_get_file_transfer () {
     global $CFG;
@@ -112,4 +178,61 @@ function codiana_get_file_transfer () {
         )
     );
     return new RemoteFileTransfer();
+}
+
+/**
+ * @return array of supported languages, key is language extension, value is language name
+ */
+function codiana_get_supported_languages () {
+    global $DB;
+    $result = $DB->get_records ('codiana_language', null, '', 'extension,name');
+    $languages = array ();
+    foreach ($result as $language)
+        $languages[$language->extension] = $language->name;
+
+    return $result;
+}
+
+/**
+ * @param $task stdClass task object
+ * @return array all registered languages for given task
+ * in array key is language extension, value is language name
+ */
+function codiana_get_task_languages ($task) {
+    global $DB;
+    $result = trim ($DB->get_field ('codiana', 'languages', array ('id' => $task->id), MUST_EXIST));
+    $result = empty($result) ? array () : explode (',', $result);
+
+    // empty result
+    if (sizeof ($result) == 0)
+        return array ();
+
+    // get all allowed languages
+    $allLanguages = codiana_get_supported_languages ();
+    $languages = array ();
+    foreach ($result as $extension) {
+        if (array_key_exists ($extension, $allLanguages))
+            $languages[$extension] = $allLanguages[$extension]->name;
+    }
+
+    return $languages;
+}
+
+/**
+ * @param $task stdClass task object
+ * @param $userID int user id
+ * @return array of all supported languages in system
+ * in array key is language extension, value is language name
+ */
+function codiana_get_all_attempts ($task, $userID) {
+    global $DB;
+    $result = $DB->get_records (
+        'codiana_attempt',
+        array (
+              'taskid' => $task->id,
+              'userid' => $userID
+        ),
+        'timesent DESC'
+    );
+    return $result;
 }
